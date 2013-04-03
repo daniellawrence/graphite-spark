c279d7d379c39523dc99146661504882a2947a40
daniellawrence <dannyla@linux.com>
2013-04-03 21:53:16 +1100

    added setup.py for python packaging

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='GraphiteSpark',
+      version='0.1',
+      description='graphitespark',
+      author='Danny Lawrence',
+      author_email='dannyla@linux.com',
+      url='http://www.github.com/daniellawrence/graphite-spark',
+      package_dir = {'': 'src'},
+      packages = [''],
+      scripts = ['bin/graphitespark']
+)
