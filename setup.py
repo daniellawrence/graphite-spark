ae816dffc4020174755be8366883427e8bbc2c3a
Daniel Lawrence <dannyla@linux.com>
2015-12-15 20:45:40 -0800

    Minor updates in setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,32 @@
 #!/usr/bin/env python
-
 from distutils.core import setup
+import textwrap
+
 
 setup(
     name='GraphiteSpark',
-    version='0.2',
+    version='0.3',
     description='graphitespark',
     author='Danny Lawrence',
     author_email='dannyla@linux.com',
     url='http://www.github.com/daniellawrence/graphite-spark',
     package_dir={'': 'src'},
     packages=[''],
+    license="GPL",
     scripts=['bin/graphitespark']
+    classifiers=textwrap.dedent("""
+    Development Status :: 5 - Production/Stable
+    Intended Audience :: Developers
+    License :: OSI Approved :: GPLv3
+    Operating System :: OS Independent
+    Programming Language :: Python :: 2.7
+    Programming Language :: Python :: 3
+    Programming Language :: Python :: 3.3
+    Programming Language :: Python :: 3.4
+    Programming Language :: Python :: 3.5
+    Topic :: Software Development :: Libraries :: Python Modules
+    Topic :: System :: Archiving :: Packaging
+    Topic :: System :: Systems Administration
+    Topic :: Utilities
+    """).strip().splitlines(),
 )
